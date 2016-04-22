ddc8564386bc60d818c5902af13a0e753bf3f6b7
james sun <sunyuyangg123@gmail.com>
2016-04-22 09:31:25 +0800

    temp

--- mocks/FakeTimeService.h
+++ mocks/FakeTimeService.h
@@ -4,9 +4,5 @@
 
 void FakeTimeService_SetMinute(int);
 void FakeTimeService_SetDay(int);
-
-void TimeService_Create(void);
-void TimeService_Destroy(void);
-
 enum {MINUTE_UNKNOWN = -1, DAY_UNKNOWN = -1 };
 #endif //TESTDRIVENDEV_FAKETIMESERVICE_H
