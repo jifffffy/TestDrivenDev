1c3c340c3073ad1d93e2a6a77d6a53d0d3f1ac2a
james sun <sunyuyangg123@gmail.com>
2016-04-22 10:06:25 +0800

    fixed LightSchedulerTest

--- tests/HomeAutomation/LightSchedulerTest.cpp
+++ tests/HomeAutomation/LightSchedulerTest.cpp
@@ -27,6 +27,13 @@ TEST_GROUP(LightScheduler)
 
 TEST(LightScheduler, CreateDoesNotChangeTheLights)
 {
-    LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
-    LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+    LONGS_EQUAL(LIGHT_ID_UNKNOW, LightControllerSpy_GetLastId());
+    LONGS_EQUAL(LIGHT_STATE_UNKNOW, LightControllerSpy_GetLastState());
 }
+
+TEST(LightScheduler, NoChangeToLightsDuringInitialization)
+{
+    LONGS_EQUAL(LIGHT_ID_UNKNOW, LightControllerSpy_GetLastId());
+    LONGS_EQUAL(LIGHT_STATE_UNKNOW, LightControllerSpy_GetLastState());
+}
+
